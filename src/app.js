ba9a5e0ae261f5f25e164bc1c9652ddaae70b302
yexianli <745569670@qq.com>
2018-10-09 22:22:29 +0800

    init

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
+
 import IndexBox from './containers/IndexBox';
-=======
-import ToDoAppContainer from './containers/ToDoAppContainer';
->>>>>>> b242a123b87a49c822019a2ba1aa24e0a8cee558
+
+
 import configureStore from './redux/configureStore';
 
 const store = configureStore();
@@ -14,11 +13,9 @@ class App extends React.Component {
   render(){
     return(
       <Provider store={store}>
-<<<<<<< HEAD
+
         <IndexBox />
-=======
-        <ToDoAppContainer />
->>>>>>> b242a123b87a49c822019a2ba1aa24e0a8cee558
+
       </Provider>
     );
   }
