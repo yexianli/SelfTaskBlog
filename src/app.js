0a47c676b9c1e43e7e1ac1e45e0e2b231de6bafa
yexianli <745569670@qq.com>
2018-10-16 10:49:19 +0800

    实现了js控制路由跳转

--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import {BrowserRouter, HashRouter, Route, withRouter} from 'react-router-dom';
 import IndexBox from './containers/IndexBox'
 import Login from './components/Login/Login'
-
+import HomeIndex from './components/HomeIndex/HomeIndex'
 import configureStore from './redux/configureStore';
 import Register from "./components/Register/Register";
 
 const store = configureStore();
 
 class App extends React.Component {
-  render(){
-    return(
-      <Provider store={store}>
+    render() {
+        return (
+            <Provider store={store}>
 
-              <div >
+                <div>
 
-                      <Route exact path="/" component={IndexBox}/>
-                      <Route path="/login" component={Login} />
-                  <Route path="/register" component={Register} />
-                  {/*<Route exact path='/' component={LoginBox}/>*/}
-                  {/*<Route path='/register' component={RegisterBox}/>*/}
-                  {/*<Route path='/home' component={HomeLayout}/>*/}
+                    <Route exact path="/" component={IndexBox}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/index" component={HomeIndex}/>
+                    {/*<Route exact path='/' component={LoginBox}/>*/}
+                    {/*<Route path='/register' component={RegisterBox}/>*/}
+                    {/*<Route path='/home' component={HomeLayout}/>*/}
 
-              </div>
+                </div>
 
-
-      </Provider>
-    );
-  }
+            </Provider>
+        );
+    }
 }
 
 ReactDOM.render(
-<HashRouter>
-    <App/>
-</HashRouter>, document.getElementById('app'));
+    <HashRouter>
+        <App/>
+    </HashRouter>, document.getElementById('app'));
 
 // 热替换HMR，需要加入这段代码才会进行生效
-if(module.hot)
+if (module.hot)
     module.hot.accept();
---
+++ src/components/HomeIndex/HomeIndex.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './HomeIndex.css'
+
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+// var url  = require('./HomeIndexPwd.png')
+
+class HomeIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+    }
+
+    componentDidMount() {
+
+    }
+
+
+
+    render() {
+        return (
+           <div>首页</div>
+        );
+    }
+}
+;
+export default HomeIndex;
